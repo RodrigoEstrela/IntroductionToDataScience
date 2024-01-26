ebf4342f07f5bf9f0e52b667b410d28367a4ac93
rdas-nev <restrela20@gmail.com>
2024-01-26 13:46:28 +0000

    mod0ex03 - fix

--- module-0/ex03/automatic_table.py
+++ module-0/ex03/automatic_table.py
@@ -1,47 +1,80 @@
 import os
-
-import pandas as pd
-from sqlalchemy import create_engine, types
+import psycopg2
 
 
-def create_table(path: str):
+def import_data(cur, table_name, csv_file_path):
     """
-    Creates a sql table based on the csv file provided by path
+    Imports the data from a csv file to a postgresql table
     """
-    db_connection = "postgresql://rdas-nev:mysecretpassword@localhost/piscineds"
-    engine = create_engine(db_connection)
 
-    csv_file_path = path
-    df = pd.read_csv(path)
+    # Check if the table already exists
+    cur.execute("SELECT EXISTS (SELECT FROM pg_tables WHERE tablename = %s)", (table_name,))
+    if cur.fetchone()[0]:
+        print(f"Table '{table_name}' already exists. Skipping import.")
+        return
 
-    table_name = csv_file_path.split("/")[-1].split(".")[0]
-    df['event_time'] = pd.to_datetime(df['event_time'])
+    # Create the table if it doesn't exist
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS {} (
+            event_time TIMESTAMP WITHOUT TIME ZONE,
+            event_type VARCHAR(255) NOT NULL,
+            product_id INTEGER,
+            price NUMERIC,
+            user_id BIGINT,
+            user_session TEXT
+            )
+    """.format(table_name))
 
-    column_types = {
-        'event_time': types.TIMESTAMP,
-        'event_type': types.VARCHAR,
-        'product_id': types.INTEGER,
-        'price': types.NUMERIC,
-        'user_id': types.BIGINT,
-        'user_session': types.UUID
-    }
+    # Open the csv file and import the data into the table
+    with open(csv_file_path, 'r') as f:
+        cur.copy_expert(
+            "COPY {} FROM STDIN WITH CSV HEADER".format(table_name),
+            f
+        )
+    print(f"Data from {csv_file_path} successfully imported into {table_name} table.")
 
-    df.to_sql(table_name, engine, index=False, if_exists='replace', dtype=column_types)
-
-    print(f"Table '{table_name}' created successfully.")
+    # Print the number of rows in the table
+    cur.execute(f"SELECT COUNT(*) FROM {table_name}")
+    row_count = cur.fetchone()[0]
+    print(f"Imported {row_count} rows into {table_name} table.")
 
 
 def main():
     """
-    Run the function create_table for every csv file on the customer dir
+    Sets db connection
+    Run the function import_data for every csv file on the customer dir
+    Commits changes
+    Closes connection
     """
-    directory_path = "customer/"
+
+    # Connection parameters
+    db_params = {
+        'host': 'localhost',
+        'port': '5432',
+        'database': 'piscineds',
+        'user': 'rdas-nev',
+        'password': 'mysecretpassword'
+    }
+
+    # Get all files on the customer dir
+    directory_path = "../../subject/customer/"
     file_list = os.listdir(directory_path)
+
+    # Establish a connection to the PostgreSQL database
+    conn = psycopg2.connect(**db_params)
+    cur = conn.cursor()
 
     for file_name in file_list:
         file_path = os.path.join(directory_path, file_name)
+        print("----------------------------------------")
         print(file_path)
-        create_table(file_path)
+        table_name = file_name.split('.')[0]
+        import_data(cur, table_name, file_path)
+        print('\n')
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
 
 
 if __name__ == '__main__':
